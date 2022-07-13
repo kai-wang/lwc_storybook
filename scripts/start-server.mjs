578a8db8f7cb331def150b2a21768ecb77302f56
Kai <simaow@outlook.com>
2022-07-13 17:27:56 +1000

    make sure the server process won't be terminated

--- scripts/start-server.mjs
+++ scripts/start-server.mjs
@@ -2,11 +2,15 @@
    
 import { createServer } from 'lwr';
 
+process.on('uncaughtException', err => {
+  console.error(err);
+});
+
 createServer()
     .listen(({ port, serverMode }) => {
         console.log(`App listening on port ${port} in ${serverMode} mode\n`);
     })
     .catch((err) => {
         console.error(err);
-        process.exit(1);
+        // process.exit(1);
     });

ee8d0a35bbc4d5d3d2a55a6396fedd6f96c48e5a
Kai <simaow@outlook.com>
2022-08-21 12:45:06 +1000

    update textInput

--- src/modules/w/textInput/textInput.js
+++ src/modules/w/textInput/textInput.js
@@ -2,17 +2,51 @@ import { LightningElement, api } from 'lwc';
 import { clsx } from 'w/utils';
 import { normalizeString, normalizeBoolean } from 'w/utilsPrivate';
 
+const SIZE = {
+  fallbackValue: 'sm',
+  validValues: ['sm', 'xl']
+};
+
 export default class TextInput extends LightningElement {
-  @api inline;
-  @api light;
-  @api readonly;
-  @api labelText;
-  @api disabled;
-  @api hideLabel;
-  @api size;
+  @api hideLabel = false;
+  @api invaild = false;
+  @api invalidText = '';
+
+  @api warn = false;
+  @api warnText;
+
+  @api light = false;
+  @api required = false;
+  @api inline = false;
+  @api readonly = false;
+  @api disabled = false;
+
+  @api helperText = '';
+  @api labelText = '';
+
+  @api value = '';
+  @api placeholder = '';
+  @api name;
+
+  _id = 'textInput-' + Math.random().toString(16);
+  _errorId = `error-${this._id}`;
+  _warnId = `warn-${this._id}`;
+  _size = SIZE.fallbackValue;
+
+  @api get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    this._size = normalizeString(value, SIZE);
+  }
 
   get hasLabelText() {
     return !!this.labelText;
+  }
+
+  get hasHelper() {
+    return !this.isFluid && !!this.helperText;
   }
 
   get computedClass() {
@@ -25,11 +59,71 @@ export default class TextInput extends LightningElement {
     );
   }
 
+  get computedLabelClass() {
+    return clsx(
+      'bx--label',
+      'bx--visually-hidden' && this.hideLabel,
+      'bx--label--disabled' && this.disabled,
+      'bx--label--inline' && this.inline,
+      `bx--label--inline-${this.size}` && this.inline
+    );
+  }
 
+  get computedHelperClass() {
+    return clsx(
+      'bx--form__helper-text',
+      'bx--form__helper-text--disabled' && this.disabled,
+      'bx--form__helper-text--inline' && this.inline
+    );
+  }
 
-  get computedLabel() {
-    if(hasLabelText) {
-      return clsx('bx--label', 'bx--visually-hidden' && this.hideLabel, 'bx--label--disabled' && this.disabled)
-    }
+  get isNotFluidHelper() {
+    return !this.isFluid && !!this.helperText;
+  }
+
+  get isNotInlineLabel() {
+    return !this.inline && !!this.labelText;
+  }
+
+  get isValidAndWarn() {
+    return !this.invaild && this.warn;
+  }
+
+  get ariaDescribedBy() {
+    return this.invaild ? this._errorId : this.warn ? this._warnId : undefined;
+  }
+
+  get isFluidNotInlineInvalid() {
+    return this.isFluid && !this.inline && this.invalid;
+  }
+
+  get isFluidNotInlineWarn() {
+    return this.isFluid && !this.inline && this.warn;
+  }
+
+  get isNormalHelper() {
+    return !this.invalid && !this.warn && !this.isFluid && !this.inline && !!this.helperText;
+  }
+
+  get isFluidInvalid() {
+    return this.isFluid && this.invalid;
+  }
+
+  get isNotFluidValidWarn() {
+    return !this.isFluid && !this.invaild && this.warn;
+  }
+
+  get computedOuterWrapper() {
+    return clsx(
+      'bx--text-input__field-outer-wrapper',
+      'bx--text-input__field-outer-wrapper--inline' && this.inline
+    );
+  }
+
+  get computedInnerWrapper() {
+    return clsx(
+      'bx--text-input__field-wrapper',
+      'bx--text-input__field-wrapper--warning' && !this.invalid && this.warn
+    );
   }
 }
